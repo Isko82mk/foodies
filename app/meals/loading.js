10b68fe20ad3c36af2db9baa5cbb030c30ae025e
isko82mk <janevski.ivica@yahoo.com>
2024-03-21 19:37:39 +0100

    update meals loading

--- app/meals/loading.js
+++
@@ -1,9 +0,0 @@
-import styles from "./loading.module.css";
-
-export default function Loader() {
-  return (
-    <div className={styles.wrapper}>
-      <p className={styles.loader}></p>
-    </div>
-  );
-}
--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,15 @@ import MealsGrid from "@/components/meals/meals-grid";
 import styles from "./page.module.css";
 import Link from "next/link";
 import getMeals from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={styles.header}>
@@ -20,7 +25,15 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={
+            <div className={styles.loaderWrapper}>
+              <p className={styles.loader}></p>
+            </div>
+          }
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
