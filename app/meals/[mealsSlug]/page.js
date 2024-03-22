fe98b9be7c29480c851aec5c02adc0b6cf235694
isko82mk <janevski.ivica@yahoo.com>
2024-03-22 16:49:08 +0100

    add error page,add details page

--- app/meals/[mealsSlug]/page.js
+++ app/meals/[mealsSlug]/page.js
@@ -1,7 +1,33 @@
-export default function MealSlug() {
+import { getMeal } from "@/lib/meals";
+import styles from "./page.module.css";
+import Image from "next/image";
+
+export default function MealSlug({ params }) {
+  const meal = getMeal(params.mealsSlug);
+
   return (
     <>
-      <h1>Shere meal</h1>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} fill alt={meal.title}></Image>
+        </div>
+        <div className={styles.headerText} s>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            {" "}
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: `${meal.instructions}`,
+          }}
+        ></p>
+      </main>
     </>
   );
 }
