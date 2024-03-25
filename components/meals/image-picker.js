ea5c5e8a32a825b8cbd21be0f23e0e5eed3df813
isko82mk <janevski.ivica@yahoo.com>
2024-03-25 22:18:25 +0100

    add share page

---
+++ components/meals/image-picker.js
@@ -0,0 +1,58 @@
+"use client";
+
+import { useRef, useState } from "react";
+import styles from "./image-picker.module.css";
+import Image from "next/image";
+export default function ImagePicker({ label, name }) {
+  //
+  const [pickedImg, setPickedImg] = useState();
+  const ImgInputRef = useRef();
+
+  const handlePickClick = () => {
+    ImgInputRef.current.click();
+  };
+  const hadleImgChange = (event) => {
+    //
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImg(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImg(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <div className={styles.preview}>
+          {!pickedImg && <p>No img picked yet</p>}
+          {pickedImg && <Image src={pickedImg} alt="user picked img" fill />}
+        </div>
+
+        <input
+          id={name}
+          className={styles.input}
+          type="file"
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={ImgInputRef}
+          onChange={hadleImgChange}
+        />
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Img
+        </button>
+      </div>
+    </div>
+  );
+}
